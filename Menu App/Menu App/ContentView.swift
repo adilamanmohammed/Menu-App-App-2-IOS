e91ac65acbf1f2b950313c6e561a2daa83b9a2d7
Adil Aman Mohammed <adil.mohammed@okstate.edu>
2024-05-28 01:39:42 -0400

    App finished

--- Menu App/Menu App/ContentView.swift
+++ Menu App/Menu App/ContentView.swift
@@ -13,26 +13,16 @@ struct ContentView: View {
     var dataService = DataService()
     
     var body: some View {
+        HStack{
+            Text("   Sushi Menu")
+                .font(.title)
+                .bold()
+                .fontWeight(.heavy)
+            Spacer()
+        }
         List(menuItems){ item in
             
-            HStack{
-                Image(item.imageName)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height: 50)
-                    .cornerRadius(10)
-                
-                Text(item.name)
-                    .bold()
-                
-                Spacer()
-                
-                Text("$"+item.price)
-                
-            }.listRowSeparator(.hidden)
-                .listRowBackground(
-                    Color(.brown).opacity(0.1)
-                )
+            MenuListRow(item: item)
         
             
         }.listStyle(.plain)
